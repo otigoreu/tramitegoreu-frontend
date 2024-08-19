e047dd4c108159c99113e7b122a2ed9561c8dabc
otigoreu <eponce@regionucayali.gob.pe>
2024-08-19 14:54:01 -0500

    pruebas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,52 +7,52 @@ import { Login2Component } from './pages/login2/login2.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
-// export const routes: Routes = [
-//   {
-//     path: '',
-//     component: PersonComponent,
-//     pathMatch: 'full',
-//     canActivate: [authGuard],
-//   },
-//   { path: 'login', component: LoginComponent, pathMatch: 'full' },
-// ];
-//53:03
 export const routes: Routes = [
   {
     path: '',
+    component: PersonComponent,
     pathMatch: 'full',
-    redirectTo: '/home',
+    canActivate: [authGuard],
   },
-  {
-    path: 'home',
-    pathMatch: 'full',
-    loadComponent: () =>
-      import('../../src/app/pages/home/home.component').then(
-        (m) => m.HomeComponent
-      ),
-  },
-  {
-    path: 'login',
-    pathMatch: 'full',
-    loadComponent: () =>
-      import('../../src/app/pages/login2/login2.component').then(
-        (m) => m.Login2Component
-      ),
-  },
-  {
-    path: 'register',
-    pathMatch: 'full',
-    loadComponent: () =>
-      import('../../src/app/pages/register/register.component').then(
-        (m) => m.RegisterComponent
-      ),
-  },
-  {
-    path: 'forgot-password',
-    pathMatch: 'full',
-    loadComponent: () =>
-      import(
-        '../../src/app/pages/forgot-password/forgot-password.component'
-      ).then((m) => m.ForgotPasswordComponent),
-  },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
 ];
+//53:03
+// export const routes: Routes = [
+//   {
+//     path: '',
+//     pathMatch: 'full',
+//     redirectTo: '/home',
+//   },
+//   {
+//     path: 'home',
+//     pathMatch: 'full',
+//     loadComponent: () =>
+//       import('../../src/app/pages/home/home.component').then(
+//         (m) => m.HomeComponent
+//       ),
+//   },
+//   {
+//     path: 'login',
+//     pathMatch: 'full',
+//     loadComponent: () =>
+//       import('../../src/app/pages/login2/login2.component').then(
+//         (m) => m.Login2Component
+//       ),
+//   },
+//   {
+//     path: 'register',
+//     pathMatch: 'full',
+//     loadComponent: () =>
+//       import('../../src/app/pages/register/register.component').then(
+//         (m) => m.RegisterComponent
+//       ),
+//   },
+//   {
+//     path: 'forgot-password',
+//     pathMatch: 'full',
+//     loadComponent: () =>
+//       import(
+//         '../../src/app/pages/forgot-password/forgot-password.component'
+//       ).then((m) => m.ForgotPasswordComponent),
+//   },
+// ];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  baseUrl: 'https://localhost:7230/api/',
+  baseUrl: 'http://168.121.236.86:7900/api/',
+  //baseUrl: 'https://localhost:7230/api/',
 };
