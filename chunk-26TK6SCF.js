f032b2947363b96092f16328fc1df7db87281def
otigoreu <eponce@regionucayali.gob.pe>
2024-08-22 16:09:12 -0500

    Auto-generated commit

--- chunk-26TK6SCF.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-2ENT3XS6.js";import"./chunk-E3PFTG2H.js";import"./chunk-REF37FEB.js";import"./chunk-AX4DNK24.js";export{a as Login2Component};
---
+++ chunk-5PYAHFJF.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-7WRNHXWK.js";import"./chunk-E3PFTG2H.js";import"./chunk-REF37FEB.js";import"./chunk-AX4DNK24.js";export{a as Login2Component};
